1d3be19ee5a7ded523083119f53524448a70fc1e
Andrey <admin@twister-vl.ru>
2020-04-23 16:06:29 +0300

    Button styles

--- react/features/notifications/constants.js
+++ react/features/notifications/constants.js
@@ -3,7 +3,7 @@
 /**
  * The standard time when auto-disappearing notifications should disappear.
  */
-export const NOTIFICATION_TIMEOUT = 2500;
+export const NOTIFICATION_TIMEOUT = 25000;
 
 /**
  * The set of possible notification types.
