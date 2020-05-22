e3cf125e35de103d83d9d61c6fca329e68ca1bab
Andrey <admin@twister-vl.ru>
2020-05-22 12:47:16 +0300

    Conference connection

--- conference-connection.js
+++ conference-connection.js
@@ -1,8 +1,24 @@
+/**
+ * Init conference controller
+ * @return {Promise<void>}
+ */
+async function init() {
+    let roomName = APP.conference.roomName;
+    console.log('Connected room', roomName);
+}
+
 (async () => {
     console.log('Conference connection init');
     try {
-        console.log(await APP.conference.getMyUserId());
-        console.log(APP.conference.roomName);
+        let interval = setInterval(function () {
+            try {
+                if(typeof APP.conference.roomName !== undefined) {
+                    clearInterval(interval);
+                    init();
+                }
+            } catch (e) {
+            }
+        }, 100);
     } catch (e) {
         console.log(e);
     }
