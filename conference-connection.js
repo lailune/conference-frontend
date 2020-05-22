f6ad5a568a8bb854ee385de1a9bd04682195e31a
Andrey <admin@twister-vl.ru>
2020-05-22 13:35:30 +0300

    Conference connection

--- conference-connection.js
+++ conference-connection.js
@@ -1,10 +1,38 @@
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+    options = { path: '/', ...options };
+    if(options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if(optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+    document.cookie = updatedCookie;
+}
+
+
 /**
  * Init conference controller
  * @return {Promise<void>}
  */
 async function init() {
     let roomName = APP.conference.roomName;
+    let authId = getCookie('aid');
+    authId = authId ? authId : '';
+
     console.log('Connected room', roomName);
+    console.log('Authorization id', authId);
 }
 
 (async () => {
