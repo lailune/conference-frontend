7f3066997d765632a88363c4f6c6d03647fbcf9c
Andrey <admin@twister-vl.ru>
2020-05-22 13:49:04 +0300

    Conference connection

--- conference-connection.js
+++ conference-connection.js
@@ -1,3 +1,5 @@
+console.log(window.location.hash);
+
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
